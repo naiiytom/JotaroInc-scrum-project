180721020a7a6747a608438aa5d986cdc8db426d
Yuttapichai Lamnaonan <y.lamnaonan@gmail.com>
2020-02-17 13:10:53 +0700

    add maintenance controller

--- public/application/controllers/Maintenance.php
+++ public/application/controllers/Maintenance.php
@@ -20,14 +20,30 @@ class Maintenance extends CI_Controller {
 		if($method != 'GET'){
 			json_output(400,array('status' => 400,'message' => 'Bad request.'));
 		} else {
-			$check_auth_client = $this->MyModel->check_auth_client();
-			if($check_auth_client == true){
-		        	$response = $this->MyModel->auth();
-		        	if($response['status'] == 200){
-		        		$resp = $this->MyModel->book_all_data();
-	    				json_output($response['status'],$resp);
-		        	}
-			}
+			$resp = $this->MyModel->maintenance_all_data();
+			json_output(200, $resp);
+		}
+    }
+
+    public function detail($id)
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method != 'GET') {
+			json_output(400,array('status' => 400,'message' => 'Bad request.'));
+        } else {
+            $resp = $this->MyModel->maintenance_detail_data($id);
+            json_output(200,$resp);
+        }
+	}
+	
+	public function create($data)
+	{
+		$method = $_SERVER['REQUEST_METHOD'];
+		if($method != 'POST') {
+			json_output(400, array('status' => 400, 'message' => 'Bad request.'));
+		} else {
+			$resp = $this->MyModel->maintenance_create_data($data);
+			json_output(200, $resp);
 		}
 	}
 }
