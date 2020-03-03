dfe22970a17f3117b13fe44dc1e85d0c4b6b3b71
Yuttapichai Lamnaonan <y.lamnaonan@gmail.com>
2020-03-03 10:36:20 +0700

    remove trash

--- public/application/views/header.php
+++ public/application/views/header.php
@@ -16,19 +16,16 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<meta name="theme-color" content="#ffffff">
 
 	<title>
-		Lazybear Backends
+		Jotaro Inc | 
+		<?php echo isset($title) ? $title : 'Jotaro Inc'; ?>
 	</title>
 
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-		integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Sarabun" rel="stylesheet">
 	<link rel="stylesheet" href="//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
-	<link rel="stylesheet" type="text/css"
-		href="https://cdnjs.cloudflare.com/ajax/libs/slim-select/1.19.0/slimselect.min.css">
-	<link rel="stylesheet"
-		href="<?php echo base_url(); ?>/static/css/main.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slim-select/1.19.0/slimselect.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>/static/css/main.css">
 
 	<script src="https://code.jquery.com/jquery-3.4.0.min.js" crossorigin="anonymous">
 	</script>
